84e1db4612848d52e28a2b717321607cea44decb
bujosa <davidbujosa@gmail.com>
2023-10-19 17:13:50 -0400

    feat: add transaction module in portfolio module

--- src/portfolio/portfolio.module.ts
+++ src/portfolio/portfolio.module.ts
@@ -4,9 +4,11 @@ import { Module } from '@nestjs/common';
 import { Portfolio, PortfolioSchema } from './repository/entities';
 import { PortfolioRepository } from './repository/repositories/portfolio.repository';
 import { PortfolioResolver } from './portfolio.resolver';
+import { TransactionModule } from 'src/transaction/transaction.module';
 
 @Module({
   imports: [
+    TransactionModule,
     MongooseModule.forFeature([
       {
         name: Portfolio.name,
