824b0bf94967ccb55e7ab62c1effb0fc6c416287
bujosa <davidbujosa@gmail.com>
2023-10-21 01:29:29 -0400

    feat: implement group transactions by asset pipeline

---
+++ src/performance-tracking/repository/pipelines/group-transactions-by-asset.pipeline.ts
@@ -0,0 +1,38 @@
+import { hexStringToObjectId } from 'src/common/functions/hex-string-to-object-id';
+
+type GroupTransactionsByAsset = {
+  portfolio: string;
+};
+
+/**
+ * @description This pipeline groups transactions by asset
+ * @param options - The options for the pipeline
+ * @returns
+ */
+export const groupTransactionsByAssetPipeline = (
+  options: GroupTransactionsByAsset,
+) => {
+  return [
+    {
+      $match: { portfolio: hexStringToObjectId(options.portfolio) },
+    },
+    {
+      $group: {
+        _id: '$asset',
+        quantity: {
+          $sum: '$quantity',
+        },
+        totalSpent: {
+          $sum: {
+            $multiply: ['$price', '$quantity'],
+          },
+        },
+      },
+    },
+    {
+      $addField: {
+        asset: '$_id',
+      },
+    },
+  ];
+};
